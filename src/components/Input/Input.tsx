e092ff58153f55b7f5df91d774dd8bad06f8ab19
thaianthai15 <nguyenanthai1562004@gmail.com>
2025-02-15 10:44:07 +0700

    Thái sửa lỗi input màu và đổi tên components Nav và Footer

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -17,7 +17,7 @@ export default function Input({ type, errorMessage, name, placeholder, register,
           type={type}
           placeholder={placeholder}
           {...register(name, rules)}
-          className='w-full pl-12 pr-4 py-2 bg-white/40 text-white border border-white/30 rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-400 placeholder-white'
+          className='w-full pl-12 pr-4 py-2 bg-white/40 text-white border border-white/30 rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-400 placeholder-white autofill:bg-transparent'
         />
         {errorMessage && <p className='text-red-300 text-xs mt-1 ml-4'>{errorMessage}</p>}
       </div>
