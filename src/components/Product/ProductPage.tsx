d2e18972456ca4d5ba8caf1cdf70f1bd5647ed7c
thaianthai15 <nguyenanthai1562004@gmail.com>
2025-03-18 00:33:32 +0700

    small fix

--- src/components/Product/ProductPage.tsx
+++ src/components/Product/ProductPage.tsx
@@ -31,28 +31,39 @@ const ProductPage: React.FC = () => {
         </p>
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-        {/* Render các sản phẩm từ danh sách baskets */}
         {baskets?.elements.map((basket) => (
           <div
             key={basket.id}
-            className='border p-4 bg-white cursor-pointer'
-            onClick={() => navigate(`/product/${basket.id}`)} // Điều hướng đến chi tiết sản phẩm
+            className='p-4 cursor-pointer relative'
+            onClick={() => navigate(`/product/${basket.id}`)}
           >
-            <div className='relative'>
-              {/* Kiểm tra nếu images tồn tại và có phần tử */}
-              {basket.images && basket.images[0] && (
-                <img className='w-full h-auto' height='300' src={basket.images[0].imageUrl} width='300' />
-              )}
+            {/* Sale Label */}
+            <div className='absolute top-10 left-4 bg-red-500 text-white text-xs font-bold py-1 px-2 rounded'>SALE</div>
+
+            {/* Product Image */}
+            {basket.images && basket.images[0] && (
+              <img
+                className='w-full h-auto rounded-lg '
+                height='300'
+                src={basket.images[0].imageUrl}
+                width='300'
+                alt={basket.name}
+              />
+            )}
+
+            {/* Product Name */}
+            <p className='mt-4 text-lg text-center font-greatvibes underline'>{basket.name}</p>
+
+            {/* Prices */}
+            <div className='text-center mt-2 flex gap-2 items-center '>
+              <p className='text-gray-500 line-through'>{basket.price + 500000} đ</p>
+              <p className='text-red-500 font-bold'>{basket.price} đ</p>
             </div>
-            <h3 className='mt-4 text-sm'>{basket.name}</h3>
-            <p className='text-xs text-gray-500'>{basket.price}</p>
-            <p className='mt-2 text-red-500 font-bold'>{basket.description} đ</p>
-            {basket.quantity && <p className='text-xs line-through'>{basket.status} đ</p>}
           </div>
         ))}
       </div>
       <div className='text-center mt-8'>
-        <button className='bg-red-500 text-white px-6 py-2 rounded-full'>XEM THÊM</button>
+        <a href='/productList' className='bg-red-500 text-white px-6 py-2 rounded-full'>XEM THÊM</a>
       </div>
     </div>
   )
